d78b07c096607b002d81786f6cd36dc04bffc220
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-07-23 23:14:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,80 @@
 document.getElementById("button").addEventListener("click",() => {
-    let t_d = [];
-    let d = [];
-    let classifying = {"male": {}, "female": {}};
-    let answers = document.getElementById("input").value.split("\n");
-    let output = [];
+    var t_d = [];
+    var d = [];
+    var occurrences = {"male": {}, "female": {}};
+    var classifying = {"male": {}, "female": {}};
+    var genres = new Set();
+    var output = [];
+
+    const answers = document.getElementById("input").value.split("\n");
 
     for (let i=0; i<answers.length; i++) {
         const answer = answers[i].split(" ");
         
-        if (answer.length == 3) { t_d.push([parseInt(answer[0]), answer[1].toLowerCase(), answer[2]]) }
+        if (answer.length == 3) { 
+            occurrences[answer[1].toLowerCase()][answer[2]] = (() => {
+               if (answer[2] in occurrences) return answer[2] + 1 
+                else return 1
+            })()
+            genres.add(answer[2])
+
+            t_d.push([parseInt(answer[0]), answer[1].toLowerCase(), answer[2]]) 
+        }
         else if (answer.length == 2) { d.push([parseInt(answer[0]), answer[1].toLowerCase()]) }
     }
+    
+    for (const genre of genres) {
+        for (const gender of ["male", "female"]) {
+            ages = [];
 
-    for (let i=0; i<t_d.length; i++) {
-        let [_, gender, genre] = t_d[i];   
-        let ages = [];
-
-        if (!(genre in classifying[gender])) {
-            for (let k=0; k<t_d.length; k++) {
-                let [k_age, k_gender, k_genre] = t_d[k]
-                
-                if ((k_gender == gender) && (k_genre == genre)) {
-                    ages.push(k_age);
+            for (const person of t_d) {
+                if (person[1] == gender && person[2] == genre) {
+                    ages.unshift(person[0]);
                 }
             }
 
-            classifying[gender][genre] = [Math.min(...ages), Math.max(...ages)]
+            if (ages.length > 0) classifying[gender][genre] = [Math.min(...ages), Math.max(...ages)]; 
         }
     }
-
-    for (let i=0; i<d.length; i++) {
-        let [age, gender] = d[i];
-        let possibilities = [];
-
-        for (let item in classifying[gender]) {
-            if (age >= classifying[gender][item][0] && age <= classifying[gender][item][1]) {
-                possibilities.push(item);
-            }
+    
+    for (const person of d) {
+        var [age, gender] = person;
+        var possibilities = []
+        
+        for (genre in classifying[gender]) {
+            let min_age = classifying[gender][genre][0];
+            let max_age = classifying[gender][genre][1];
+                
+            if (age <= max_age && age >= min_age) possibilities.unshift(genre);
         }
 
-        for (let i=1; i<possibilities.length; i++) {
-            let x = possibilities[i-1];
-            let y = possibilities[i];    
-            let min_x = classifying[gender][x][0];
-            let min_y = classifying[gender][y][0];
-            let differences = [age-min_x, age-min_y];
-            
-            if (differences[0] != differences[1]) {
-                let maximum = Math.max(...differences)
+        for (const k of possibilities.slice(1)) {
+            const [min_a, min_b, max_a, max_b] = [
+                classifying[gender][possibilities[0]][0],
+                classifying[gender][possibilities[0]][1],
+                classifying[gender][k][0],
+                classifying[gender][k][1],
+            ]
 
-                if (maximum == differences[0]) {
-                    possibilities.splice(possibilities.indexOf(x), 1)
-                }
+            const [diff_a, diff_b, diff_c, diff_d] = [Math.abs(age-min_a), Math.abs(age-min_b), Math.abs(age-max_a), Math.abs(age-max_b)]
+            const occ_a = occurrences[gender][possibilities[0]]
+            const occ_b = occurrences[gender][k]
 
-                else {
-                    possibilities.splice(possibilities.indexOf(y), 1)
-                }
+            if (!(diff_a == diff_b)) {
+                possibilities.splice(possibilities.indexOf([possibilities[0], k][Math.max(...[max_a, max_b])==max_b], 1))
+            }
+    
+            else if (!(occ_a == occ_b)) {
+                possibilities.splice([possibilities[0],k][Math.min(...[occ_a, occ_b])==occ_b])
             }
 
             else {
-                occurrences_x = 0;
-                occurrences_y = 0;
-
-                for (let k=0; k<t_d.length; k++) {
-                    if (t_d[k][2] == x) {
-                        occurrences_x++
-                    }
-
-                    else if (t_d[k][2] == y) {
-                        occurrences_y++
-                    }
-                }
-                let minimum = Math.min(...[occurrences_x, occurrences_y]);
-                
-                if (minimum==occurrences_x) {
-                    possibilities.splice(possibilities.indexOf(x), 1)
-                }
-                
-                else {
-                    possibilities.splice(possibilities.indexOf(y), 1)
-                }
-
+                possibilities.splice([possibilities[0], k][Math.max(...[max_c, max_d])==max_c],1)
             }
         }
 
         output.push(age + " " + gender + " " + ":" + " " + possibilities.join("/"));
     }
-
+    
     document.getElementById("output").innerHTML = output.join("\n");
 })
