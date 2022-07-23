2a3130ccc741750795d4829f061c0ef7ab3c713f
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-07-23 23:37:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 document.getElementById("button").addEventListener("click",() => {
     var t_d = [];
     var d = [];
-    var occurrences = {"male": {}, "female": {}};
+    var occurrences = {};
     var classifying = {"male": {}, "female": {}};
     var genres = new Set();
     var output = [];
- 
+
     const answers = document.getElementById("input").value.split("\n");
 
     for (let i=0; i<answers.length; i++) {
         const answer = answers[i].split(" ");
         
         if (answer.length == 3) { 
-            occurrences[answer[1].toLowerCase()][answer[2]] = (() => {
-               if (answer[2] in occurrences) return answer[2] + 1 
-                else return 1
-            })()
+            if (answer[2] in occurrences) occurrences[answer[2]] += 1
+            else occurrences[answer[2]] = 1
             genres.add(answer[2])
 
             t_d.push([parseInt(answer[0]), answer[1].toLowerCase(), answer[2]]) 
         }
         else if (answer.length == 2) { d.push([parseInt(answer[0]), answer[1].toLowerCase()]) }
     }
-    
+
     for (const genre of genres) {
         for (const gender of ["male", "female"]) {
             ages = [];
@@ -49,19 +47,16 @@ document.getElementById("button").addEventListener("click",() => {
         }
 
         for (const k of possibilities.slice(1)) {
-            const [min_a, min_b, max_a, max_b] = [
-                classifying[gender][possibilities[0]][0],
-                classifying[gender][possibilities[0]][1],
-                classifying[gender][k][0],
-                classifying[gender][k][1],
-            ]
-
+            const min_a = classifying[gender][possibilities[0]][0];
+            const min_b = classifying[gender][k][0];
+            const max_a = classifying[gender][possibilities[0]][1];
+            const max_b = classifying[gender][k][1];
             const [diff_a, diff_b, diff_c, diff_d] = [Math.abs(age-min_a), Math.abs(age-min_b), Math.abs(age-max_a), Math.abs(age-max_b)]
-            const occ_a = occurrences[gender][possibilities[0]]
-            const occ_b = occurrences[gender][k]
-
+            const occ_a = occurrences[possibilities[0]]
+            const occ_b = occurrences[k]
+            
             if (!(diff_a == diff_b)) {
-                possibilities.splice(possibilities.indexOf([possibilities[0], k][Math.max(...[max_a, max_b])==max_b], 1))
+                possibilities.splice(possibilities.indexOf([possibilities[0], k][Math.max(...[diff_a, diff_b])==diff_b], 1))
             }
     
             else if (!(occ_a == occ_b)) {
@@ -69,7 +64,7 @@ document.getElementById("button").addEventListener("click",() => {
             }
 
             else {
-                possibilities.splice([possibilities[0], k][Math.max(...[max_c, max_d])==max_c],1)
+                possibilities.splice([possibilities[0], k][Math.max(...[diff_c, diff_d])==diff_c],1)
             }
         }
 
