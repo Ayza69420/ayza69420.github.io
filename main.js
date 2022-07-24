a852c91a5ec7fa6c88666d9ad297ccc9d89fbd47
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-07-24 05:53:11 +0300

    Fully remade
    >Optimization
    >Improved algorithm
    >Better use of JS features

--- main.js
+++ main.js
@@ -45,31 +45,32 @@ document.getElementById("button").addEventListener("click",() => {
                 
             if (age <= max_age && age >= min_age) possibilities.unshift(genre);
         }
-
+        
         for (const k of possibilities.slice(1)) {
             const min_a = classifying[gender][possibilities[0]][0];
             const min_b = classifying[gender][k][0];
             const max_a = classifying[gender][possibilities[0]][1];
             const max_b = classifying[gender][k][1];
-            const [diff_a, diff_b, diff_c, diff_d] = [Math.abs(age-min_a), Math.abs(age-min_b), Math.abs(age-max_a), Math.abs(age-max_b)]
+            const differences = [Math.abs(age-min_a), Math.abs(age-min_b), Math.abs(age-max_a), Math.abs(age-max_b)]
             const occ_a = occurrences[possibilities[0]]
             const occ_b = occurrences[k]
             
-            if (!(diff_a == diff_b)) {
-                possibilities.splice(possibilities.indexOf([possibilities[0], k][Math.max(...[diff_a, diff_b])==diff_b], 1))
+            if (!(differences[0] == differences[1])) {
+                possibilities.splice(possibilities.indexOf(Math.max(...differences.slice(0,2))==differences[1]?k:possibilities[0]), 1)
             }
-    
+
             else if (!(occ_a == occ_b)) {
-                possibilities.splice([possibilities[0],k][Math.min(...[occ_a, occ_b])==occ_b])
+                possibilities.splice(possibilities.indexOf(Math.min(occ_a, occ_b)==occ_b?k:possibilities[0]) ,1)
             }
 
             else {
-                possibilities.splice([possibilities[0], k][Math.max(...[diff_c, diff_d])==diff_c],1)
+                possibilities.splice(possibilities.indexOf(Math.max(...differences.slice(3,5))==differences[3]?k:possibilities[0]) ,1)
             }
+
         }
 
         output.push(age + " " + gender + " " + ":" + " " + possibilities.join("/"));
+        
+        document.getElementById("output").innerHTML = output.join("\n");
     }
-    
-    document.getElementById("output").innerHTML = output.join("\n");
 })
